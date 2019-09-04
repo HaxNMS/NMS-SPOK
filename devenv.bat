7a667eaf3c3bc8e17e59f22b769e8a2de2e0a285
Gaticus Hax <gaticus.hax@gmail.com>
2019-09-03 22:02:20 -0700

    update devenv.bat

--- devenv.bat
+++ devenv.bat
@@ -3,36 +3,35 @@
 call :IsSet INCLUDE_DEVENV && goto :End
 set INCLUDE_DEVENV=1
 
-:: if the first argument is -Q or ---QUIET
-set _ARG_=%~1
-call "%~dp0\scripts\common\to-upper.bat" _ARG_
-set _OPT_QUIET=&rem unset
-if "%_ARG_%"=="-Q"      set _OPT_QUIET_=1
-if "%_ARG_%"=="--QUIET" set _OPT_QUIET_=1
-set _ARG_=
-
-call :IsSet _OPT_QUIET_ && shift
+:: if the first argument is -Q or --QUIET
+set _OPT_QUIET_=%~1
+call :GetOption _OPT_QUIET_ -Q --QUIET && shift
 call :IsSet _OPT_QUIET_ || setlocal
 
 set NMS_PATH=U:\SteamLibrary\steamapps\common\No Man's Sky
 set MOD_PATH=%NMS_PATH%\GAMEDATA\PCBANKS\MODS
 
-set NMSTools=X:\Media\Games\No Mans Sky\Tools
-set HaxTools=%NMSTools%\HaxTools
+set HAXTOOLS=%~dp0\modules\HaxTools
 
-set MBINC_PATH=X:\Media\Games\No Mans Sky\Tools\MBINCompiler\Build\Release
-
-set PATH=%PATH%;%HaxTools%;%HaxTools%\bin
-set PATH=%PATH%;%NMSTools%\google-shaderc\bin
-set PATH=%PATH%;%MBINC_PATH%
-set PATH=%PATH%;%~dp0%\scripts
-
-doskey mbinc=MBINCompiler.exe $*
+set PATH=%PATH%;%~dp0\scripts
+set PATH=%PATH%;%HAXTOOLS%
+set PATH=%PATH%;%HAXTOOLS%\bin
+set PATH=%PATH%;%~dp0\modules\google-shaderc\bin
 
 call :IsSet _OPT_QUIET_ && goto :End
 
 set _TITLE_=NMS Shader Development Kit
 cmd /K "@title %_TITLE_%&@echo]%_TITLE_%&echo]"
+
+:GetOption
+    :: set "VAR=%~1" & set "SHORT=%~2" & set "LONG=%~3"
+    call "%~dp0\scripts\common\to-upper.bat" %1
+    call set VAL=%%%1%%
+    if "%VAL%"=="%2" set %1=%3
+    if not "%VAL%"=="%3" set %1=
+    set VAL=
+    call :IsSet %1
+    exit /b
 
 :IsSet
     set %~1 1>nul 2>&1
